4b6ecbe42bb4725b56c491bd70e88e04591253d8
Guilherme Santos <agustinho_santos@hotmail.com>
2022-07-31 23:09:23 -0300

    node server

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const http = require('http');
+const url = require('url');
+const fs = require('fs');
 
 http.createServer((request, response) => {
 
-  response.writeHead(200, { 'Content-Type': 'application/json' });
-  response.end(JSON.stringify({ text: "<h1>Hello World!</h1>"}));
+  let path = url.parse(request.url).pathname;
+
+  if(path == "" || path == "/"){
+    path = "/index.html";
+  }
+  let fileName = "." + path;
+
+  fs.readFile(fileName, (err, data) => {
+    if(err){
+      response.writeHead(404, { "Content-Type": "text/html;charset=utf-8" });
+      response.end("<h1>Página não Encontrada</h1>");
+    }else{
+      response.writeHead(200, { "Content-Type": "text/html;charset=utf-8" });
+      response.write(data);
+      response.end();
+    }
+  })
 
 }).listen(3000, (err) => {
   if (err) {
